10e9cb6f0220ed6b61f2213e5134974a33bbc426
Alejandro Laguna <alejandro.laguna@epitech.eu>
2025-01-15 19:48:48 +0100

    feat: new colors

--- lua/noctis/palette.lua
+++ lua/noctis/palette.lua
@@ -1,18 +1,18 @@
 local variants = {
 	main = {
-		placeholder = "#FFFF00",
-		color100 = "#FAFAFA",
-		color200 = "#D4D4D4",
-		color300 = "#A9A9A9",
-		color400 = "#696969",
-		color500 = "#505050",
-		color600 = "#303030",
-		color700 = "#151515",
-		color800 = "#000000",
-		primary = "#887eaa",
-		success = "#7EAB8E",
-		warning = "#FFF2AF",
-		error = "#F57A7A",
+		placeholder = "#D0D0F3", -- Soft starlight glow
+		color100 = "#E2E6F5", -- Pale nebula mist
+		color200 = "#A3A9E1", -- Dusty space cloud
+		color300 = "#6A6B95", -- Twilight sky
+		color400 = "#373B6F", -- Deep space blue
+		color500 = "#1A2049", -- Dark nebula
+		color600 = "#0D1128", -- Black hole shadows
+		color700 = "#020616", -- Infinite dark space
+		color800 = "#000000", -- Absolute darkness
+		primary = "#4B3C7B", -- Cosmic purple
+		success = "#5D9E75", -- Glowing green star
+		warning = "#F4E42C", -- Solar flare yellow
+		error = "#D14A6C", -- Supernova red
 		none = "none",
 	},
 }
